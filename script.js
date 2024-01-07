0acf7bb2a7c022747c36eca336f39b9f63e4c16b
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-07 21:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Add event listener to reset color after 1 second
     square.addEventListener("mouseenter", function () {
-      square.style.backgroundColor = "red";
+      square.style.backgroundColor = getRandomColor();
       setTimeout(() => {
         square.style.backgroundColor = "yellowgreen";
       }, 1000);
     });
   }
+	
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 });
